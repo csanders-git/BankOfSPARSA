52e7452d2ea2f3f412242527275a1ebba6da43a5
csanders-git <csanders@sparsa.org>
2016-02-25 18:31:05 -0500

    updated example files

--- exampleWhite.py
+++ exampleWhite.py
@@ -4,9 +4,9 @@ import sys # for testing
 
 url = "http://52.90.140.22:5000"
 args = {}
-args['accountNum'] = "0112345678"
+args['accountNum'] = "0112345679"
 args['password'] = "test3"
-#args['challenge'] = "H`:L]oo$M5"
+args['challenge'] = "123"
 loc = "/getSession"
 response = requests.post(url+loc,data=args)
 decodedOut = json.loads(response.text)
@@ -14,12 +14,13 @@ decodedOut = json.loads(response.text)
 session = None
 if isinstance(decodedOut, dict):
     session = decodedOut['SessionID']
+    print session
 else:
     print decodedOut
     sys.exit()
 
 args = {}
-args['accountNum'] = "0112345678"
+args['accountNum'] = "0112345679"
 args['session'] = session
 loc = "/getBalance"
 response = requests.post(url+loc,data=args)
@@ -33,18 +34,16 @@ else:
 
 args = {}
 args['accountNum'] = "0112345678"
-args['password'] = "test3"
 args['session'] = session
 args['newPassword'] = "test3"
 loc = "/changePassword"
 response = requests.post(url+loc,data=args)
-
 decodedOut = json.loads(response.text)
 
 if isinstance(decodedOut, dict):
 	print decodedOut['Status']
 else:
-	print decodedOut 
+	print decodedOut
 
 args = {}
 args['destAccount'] = "0112345678"
@@ -57,14 +56,13 @@ decodedOut = json.loads(response.text)
 if isinstance(decodedOut, dict):
 	print decodedOut['Status']
 else:
-	print decodedOut
-    
-    
+	print decodedOut         
+
 args = {}
 args['accountNum'] = "0112345678"
-args['destAccount'] = "0112345678"
+args['destAccount'] = "0112345679"
 args['session'] = session
-args['amount'] = "108"
+args['amount'] = "1"
 loc = "/transferMoney"
 response = requests.post(url+loc,data=args)
 decodedOut = json.loads(response.text)
@@ -72,13 +70,13 @@ decodedOut = json.loads(response.text)
 if isinstance(decodedOut, dict):
 	print decodedOut['Status']
 else:
-	print decodedOut
+	print decodedOut           
+
 
 args = {}
 args['accountNum'] = "0112345678"
 args['session'] = session
 args['newPin'] = "1234"
-args['pin'] = "1235"
 loc = "/changePin"
 response = requests.post(url+loc,data=args)
 decodedOut = json.loads(response.text)
@@ -86,4 +84,4 @@ decodedOut = json.loads(response.text)
 if isinstance(decodedOut, dict):
 	print decodedOut['Status']
 else:
-	print decodedOut      
+	print decodedOut    
